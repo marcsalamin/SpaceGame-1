8a06f613dabff33ac77b1e5f5a833c19ca24d149
phili <phili@10.91.3.81>
2018-05-28 13:04:32 +0200

    enemy

--- src/objects/EnemySpaceShip.java
+++ src/objects/EnemySpaceShip.java
@@ -5,11 +5,13 @@ import java.awt.Point;
 import javafx.scene.shape.Circle;
 
 public class EnemySpaceShip {
+	
 	Point p;
 	float Vx, Vy;
 	private  int hp ;
 	Circle c;
 	double hitBox;
+	
 	EnemySpaceShip(Point p,float Vx, float Vy,double hitBox,int hp){
 		this.p = p;
 		this.Vx = Vx;
@@ -17,10 +19,11 @@ public class EnemySpaceShip {
 		this.c= new Circle(p.getX(),p.getY(),hitBox);
 	}
 	
-	public  Point getPosition(){
-		return this.p;
+	public Point getPosition(){
+		return p;
 	}
-	public  void helthDown(){
+	
+	public void helthDown(){
 		this.hp--;
 	}
 	
