8a9a6d626ce1535a3f3ac59a2bfd91b6f4a21033
phili <phili@10.91.3.81>
2018-05-28 14:03:27 +0200

    # WARNING: head commit changed in the meantime
    Test commit

--- src/objects/Bullets.java
+++ src/objects/Bullets.java
@@ -6,20 +6,24 @@ import game.GameCode;
 import javafx.scene.shape.Circle;
 
 public class Bullets {
+	
 	enum State  {FRIEND, ENEMY};
+	
 	State s;
 	Point p;
 	float Vx;
 	float Vy;
 	double hitBox = GameCode.height / 40;
 	Circle c;
+	int damage;
 	 
-	public Bullets(Point p, float Vx, float Vy , State s){ 
+	public Bullets(Point p, float Vx, float Vy , State s, int damage){ 
 		 this.p = p;
 		 this.Vx = Vx;
 		 this.Vy = Vy;
 		 this.c= new Circle(p.getX(),p.getY(),hitBox);
 		 this.s = s;
+		 this.damage = damage;
 	 }
 
 }
