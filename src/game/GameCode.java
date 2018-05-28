cd1c2476ed9b9410ff6376d08cf40829c4f66790
Marc <marc.salamin.ch@gmail.com>
2018-05-28 13:51:00 +0200

    again

--- src/game/GameCode.java
+++ src/game/GameCode.java
@@ -2,23 +2,44 @@ package game;
 
 import java.awt.Dimension;
 import java.awt.Point;
+import java.util.Vector;
 
+import ch.hevs.gdx2d.desktop.PortableApplication;
+import ch.hevs.gdx2d.lib.GdxGraphics;
+import objects.Bullets;
 import objects.EnemySpaceShip;
  
 
-public class GameCode {
+public class GameCode extends PortableApplication {
 	Point p;
+	Vector <Bullets> b = new Vector<Bullets>();
+	Vector <EnemySpaceShip> ves = new Vector <EnemySpaceShip>();
 
 	
 	
 	public void generateEnemey(){
 		p.setLocation(Math.random()*width, height);
 		EnemySpaceShip es = new EnemySpaceShip( p, (float) 0,(float) height/20, width/40, 1);
+		ves.add(es);
 	}
 
 
 	static Dimension dimension = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
 	public static double height = dimension.getHeight();
 	public static double width  = dimension.getWidth();
+
+
+
+	@Override
+	public void onInit() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void onGraphicRender(GdxGraphics g) {
+		// TODO Auto-generated method stub
+		g.drawCircle(40, 30, 50);
+	}
 
 }
