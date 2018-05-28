adfee4a7335cf110f828cd6f40bc07bbd5313450
Keereslame <34709863+Keereslame@users.noreply.github.com>
2018-05-28 15:36:47 +0200

    Keereslame
    maj

--- src/game/Collision.java
+++ src/game/Collision.java
@@ -5,16 +5,20 @@ import java.awt.Point;
 public class Collision {
 	
 	public void tick(){
+		Point p3 = GameCode.os.getPosition();
+		float h3 = GameCode.os.getHitBox();
+		
 		for (int i =0; i < GameCode.b.size();i++){
-			for(int j = 0; j< GameCode.ves.size();i++){
-				Point p1 = GameCode.b.get(i).getPosition(); 
-				Point p2 = GameCode.ves.get(j).getPosition(); 
-				Point p3 = GameCode.os.getPosition();
-				float h1 = GameCode.b.get(i).getHitBox();
-				float h2 = GameCode.ves.get(j).getHitBox();
-				float h3 = GameCode.os.getHitBox();
+			for(int j = 0; j< GameCode.ves.size();j++){
 				
-				if(p1.distance(p2)<h1+h2){
+				Point p1 = GameCode.b.get(i).getPosition(); 
+				float h1 = GameCode.b.get(i).getHitBox();
+				
+				Point p2 = GameCode.ves.get(j).getPosition(); 
+				float h2 = GameCode.ves.get(j).getHitBox();
+				
+				
+				if(p1.distance(p2)<(h1+h2)){
 					GameCode.b.removeElementAt(i);
 					GameCode.ves.get(j).helthDown();
 					if(GameCode.ves.get(j).getHealth()<1){
@@ -22,7 +26,7 @@ public class Collision {
 					}
 					
 				}
-				if(p1.distance(p3)<h1+h3){
+				if(p1.distance(p3)<(h1+h3)){
 					GameCode.b.removeElementAt(i);
 					GameCode.os.helthDown();
 					if(GameCode.os.getHealth()<1){
