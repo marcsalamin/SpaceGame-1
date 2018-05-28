7ad55ad432cd70b025f656104a53421bfd1d0208
Keereslame <34709863+Keereslame@users.noreply.github.com>
2018-05-28 15:10:35 +0200

    Keereslame
    maj gameCode (affichage)

--- src/game/GameCode.java
+++ src/game/GameCode.java
@@ -16,19 +16,23 @@ import sun.java2d.pipe.OutlineTextRenderer;
 
 public class GameCode extends PortableApplication {
 	static Dimension dimension = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
-	public static double height = dimension.getHeight();
-	public static double width  = dimension.getWidth();
+	public static float height = (float) dimension.getHeight();
+	public static float width  = (float) dimension.getWidth();
 	
 	static Vector <Bullets> b = new Vector<Bullets>();
 	static Vector <EnemySpaceShip> ves = new Vector <EnemySpaceShip>();
 	static OurSpaceShip os;
+	
+	public GameCode(){
+		super((int)width,(int) height,true);
+	}
 
 	public void generateEnemy(){
-		EnemySpaceShip es = new EnemySpaceShip(new Point((int)(Math.random()*width),(int) height), (float) 0,(float) height/20, width/40, 1);
+		EnemySpaceShip es = new EnemySpaceShip(new Point((int)(Math.random()*width),(int) (3*height/4)), (float) 0,(float) height/20, width/40, 1);
 		ves.add(es);
 	}
 	public void generateOurSpaceShip(){
-		os = new OurSpaceShip(new Point((int)width/2,0), (float) 0,(float)0,width/40, 3);
+		os = new OurSpaceShip(new Point((int)width/2,(int)height/4), (float) 0,(float)0,width/40, 3);
 	}
 
 
@@ -40,10 +44,12 @@ public class GameCode extends PortableApplication {
 	
 	@Override
 	public void onGraphicRender(GdxGraphics g) {
-		for(int i = 0; i<= ves.size(); i++){
-			g.drawCircle((float)ves.get(i).getPosition().getX(), (float)ves.get(i).getPosition().getY(), (float)ves.get(i).getHitBox(), Color.RED);
+		g.clear(Color.WHITE);
+		
+		for(int i = 0; i< ves.size(); i++){
+			g.drawCircle((float)ves.get(i).getPosition().getX(),(float) ves.get(i).getPosition().getY(), (float)ves.get(i).getHitBox(), Color.RED);
 		}
-		g.drawCircle((float)os.getPosition().getX(),(float) os.getPosition().getY(),(float)os.getHitBox());
+		g.drawCircle((float)os.getPosition().getX(),(float) os.getPosition().getY(),(float)os.getHitBox(), Color.BLUE);
 	}
 	
 	public static void main(String[] args) {
